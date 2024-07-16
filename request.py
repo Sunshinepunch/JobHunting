ef36f94f54df5250a8b9186b4425feab71dc9247
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2024-07-16 15:55:39 -0700

    adds url param

--- request.py
+++ request.py
@@ -1,16 +1,17 @@
 import requests
+URL = ""
 
 #make a session object (improves performance by reusing TCP connection)
-s = requests.Session()
+#s = requests.Session()
 
 # Making a GET request
 #response = requests.get('https://api.github.com/users/Sunshinepunch')
 
-s.get('https://httpbin.org/cookies/set/sessioncookie/123456789')
+#s.get('https://httpbin.org/cookies/set/sessioncookie/123456789')
 
-response = s.get('https://httpbin.org/cookies')
+response = requests.get(URL)
 
-print (response.text)
+print (response.content)
 # check status code for response received
 # success code - 200
 #print(response.url)
